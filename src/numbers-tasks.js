a8c139452c5306fa6a95b701a8a8d3bfabf6531f
Breslavets Maria <alwayslearn@mail.ru>
2024-12-25 21:34:16 +0300

    feat: solve the getLastDigit and parseNumberFromString problem

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -108,7 +108,7 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (0,1)     => 0
  */
 function getAngleBetweenVectors(x1, y1, x2, y2) {
-  return Math.atan2(x2 - x1, y2 - y1);
+  return (180 * Math.atan2(x2 - x1, y2 - y1)) / Math.PI;
 }
 
 // cosα=ax·bx+ay·by/ax2+ay2·bx2+by2
@@ -126,8 +126,11 @@ function getAngleBetweenVectors(x1, y1, x2, y2) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  if (value >= 10) {
+    return value % 10;
+  }
+  return value;
 }
 
 /**
@@ -141,8 +144,8 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return value * 1;
 }
 
 /**
